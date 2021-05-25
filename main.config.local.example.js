6ad3ca2f2d6e3f17300dc7bd2ebef1c7b1a0e5d0
MaxQNEI <maxqnei@gmail.com>
2021-05-25 20:31:44 +0300

    Add main.config.local.example.js

---
+++ main.config.local.example.js
@@ -0,0 +1,16 @@
+module.exports = {
+    Mongo: {
+        url: 'mongodb://localhost',
+        db: 'mrmr',
+    },
+
+    Express: {
+        host: 'localhost',
+        port: 8080,
+    },
+
+    Socket: {
+        host: 'localhost',
+        port: 8081,
+    },
+};
